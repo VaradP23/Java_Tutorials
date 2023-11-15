098ee80b90ef48c48fffb6879d5a6af177aaf7a8
VaradP23 <150345838+VaradP23@users.noreply.github.com>
2023-11-15 17:36:48 +0530

    Delete FirstJavaClass.java

--- FirstJavaClass.java
+++
@@ -1,8 +0,0 @@
-public class FirstJavaClass {
-    public static void main(String[] args){
-        int dateOfBirth = 28042001;
-        System.out.println("Hello World");
-
-        System.out.println("Birthdate :" + dateOfBirth);
-    }
-}
